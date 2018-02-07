485f2c692691953a26e15aa2295438304d29beaf
Mahesh Rasal <maheshr@cpbs.net>
2018-02-07 12:18:17 +0530

    dfsf

--- app.js
+++ app.js
@@ -171,11 +171,13 @@ if (req.body.result.action ==='software')
 return res.json(facebookResponse);
 }
        if(req.body.result.action=== 'network.network-custom'){
-        // var desc1=req.body.contexts.parameters.desc
-        firstapp.logIncident('desc 1234','',function(err,resu)
+        // =req.body.contexts.parameters.desc
+        var desc1=req.body.result.parameters.desc
+        firstapp.logIncident(desc1,'',function(err,resu)
         {
           success=resu["result"]["number"];
-          var resagent='Your incident id is generated with Incident number : '+success;
+        
+          var resagent='Your incident id is generated with Incident number : '+success +desc1;
           console.log('request are'+resagent);
            return res.json({
              speech:resagent,
