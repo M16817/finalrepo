55ee75c5e4f39732ea3e5364bf007a683ee2f5d5
Mahesh Rasal <maheshr@cpbs.net>
2018-03-06 16:55:55 +0530

    ddd

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '737361746461603', // your App ID
         'clientSecret'  : 'e71ecb9ee8c1a8c770cf21b0d0283a9a', // your App Secret
-        'callbackURL'   : 'https://mydemoflight.herokuapp.com/callback', //http://localhost:8080/auth/facebook/callback',
+        'callbackURL'   : 'https://mydemoflight.herokuapp.com/fb/callback', //http://localhost:8080/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
     },
@@ -20,7 +20,7 @@ module.exports = {
      'googleAuth' : {
          'clientID'      : '764670484091-ibj46k3nqemah8on4kb8l4vmqbjotu4b.apps.googleusercontent.com',
          'clientSecret'  : 'aN_vBm_e26XsmMUs10H-eCAg',
-         'callbackURL'   : 'htthttps://mydemoflight.herokuapp.com/googlecallback'
+         'callbackURL'   : 'https://mydemoflight.herokuapp.com/ga/callback'
      }
 
 };
