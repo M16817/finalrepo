2f4a5fbc5f13d9ecf6b2e53a41692c1bd3eb409e
Mahesh Rasal <maheshr@cpbs.net>
2018-03-19 11:42:55 +0530

    commit

--- restapimethods.js
+++ restapimethods.js
@@ -69,7 +69,7 @@ module.exports = {
       method: 'GET',
       url: 'https://graph.facebook.com/me',
       qs: { PSID : recid,
-         access_token : 'EAAKeoGj4A6MBAME63jlRCpMpPYyZCQZCDml5kK9ZChj07vhdvoZBWJPFLjci0fuVooYR0MRsrTDr5XfOoTXxeZBCIUZAr1r7KLiypeJPib1BylLbtyMnS8hrpdWGrWZBeveZAO5vZBwFd10EZAm4dN2bNM9nhdnZCZAr0BaX3sH497ppeAw4ZCuy3AbqVhC5vzpxI0CWWxo1vVF1FsAZDZD'}
+         access_token : '737361746461603|CIYfW6g0Aef1u4yxdUCXM5LWMcI'}
     };
 
     request(options, function (error, response, body) {
